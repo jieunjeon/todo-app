303763b941ec55a9e1953d4e2f7a076dd6b31e4f
Jieun Jeon <jieunjeon1023@gmail.com>
2019-08-07 16:14:16 +0900

    add edit-task ui

--- App.js
+++ App.js
@@ -1,62 +1,64 @@
-import React from 'react';
-import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform, ScrollView } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TextInput,
+  Dimensions,
+  Platform,
+  ScrollView
+} from "react-native";
 import ToDo from "./ToDo";
 
-const {height, width} = Dimensions.get("window");
+const { height, width } = Dimensions.get("window");
 
 export default class App extends React.Component {
-
-  state= {
+  state = {
     newToDo: ""
-  }
-
+  };
   render() {
-
-    const {newToDo} = this.state;
-
-  
-
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle="light-content" />
-      <Text style={styles.title}>Todo</Text>
-      <View style={styles.card}>
-        <TextInput 
-          style={styles.input} 
-          placeholder={"New To Do"}
-          value={newToDo} 
-          onChangeText={this._controlNewTodo}
-          placeholderTextColor={"#999"}
-          returnKeyType={"done"}
-          autoCorrect={false}
-        />
-        <ScrollView contentContainerStyle={styles.toDos}>
-          <ToDo />
-        </ScrollView>
+    const { newToDo } = this.state;
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>To Do</Text>
+        <View style={styles.card}>
+          <TextInput
+            style={styles.input}
+            placeholder={"New To Do"}
+            value={newToDo}
+            onChangeText={this._crontollNewToDo}
+            placeholderTextColor={"#999"}
+            returnKeyType={"done"}
+            autoCorrect={false}
+          />
+          <ScrollView contentContainerStyle={styles.toDos}>
+            <ToDo text={"Hello I'm a To Do"} />
+          </ScrollView>
+        </View>
       </View>
-    </View>
-  );
-}
-
-  _controlNewTodo = text => {
+    );
+  }
+  _crontollNewToDo = text => {
     this.setState({
       newToDo: text
-    })
-  }
+    });
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F23657',
-    alignItems: 'center',
+    backgroundColor: "#F23657",
+    alignItems: "center"
   },
-  title:{
+  title: {
     color: "white",
     fontSize: 30,
     marginTop: 50,
-    fontWeight: "400",
-    marginBottom: 50,
+    fontWeight: "200",
+    marginBottom: 30
   },
   card: {
     backgroundColor: "white",
@@ -83,9 +85,9 @@ const styles = StyleSheet.create({
     padding: 20,
     borderBottomColor: "#bbb",
     borderBottomWidth: 1,
-    fontSize: 25,
+    fontSize: 25
   },
   toDos: {
-    alignItems: "center",
+    alignItems: "center"
   }
 });
